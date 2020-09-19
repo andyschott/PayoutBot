b8736c71060b21e3c68665416f4be4a58600546d
Andy Schott <andy@andyschott.com>
2020-09-19 14:05:46 -0400

    add logging

--- PayoutBot.Discord/DiscordClient.cs
+++ PayoutBot.Discord/DiscordClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PayoutBot.Discord.Configuration;
 using PayoutBot.Discord.Models;
@@ -14,15 +15,16 @@ namespace PayoutBot.Discord
     {
         private readonly DiscordSocketClient _client = new DiscordSocketClient();
         private readonly IOptions<BotConfig> _config;
-
+        private readonly ILogger<DiscordClient> _logger;
         private IMessageChannel _writeChannel = null;
         private IUserMessage _message = null;
 
         private const string Thumbnail = "https://www.bobasalliance.com/wp-content/uploads/2020/01/bobasbot.png";
 
-        public DiscordClient(IOptions<BotConfig> config)
+        public DiscordClient(IOptions<BotConfig> config, ILogger<DiscordClient> logger)
         {
             _config = config;
+            _logger = logger;
         }
 
         public async Task Login()
@@ -36,6 +38,7 @@ namespace PayoutBot.Discord
         {
             if(_message == null)
             {
+                _logger.LogInformation("No message found in channel, returning");
                 return;
             }
 
@@ -72,20 +75,33 @@ namespace PayoutBot.Discord
             });
             builder.Fields = fields.ToList();
 
-            await _message.ModifyAsync(props =>
+            try
             {
-                props.Embed = builder.Build();
-            });
+                _logger.LogInformation("Updating message in Discord");
+                await _message.ModifyAsync(props =>
+                {
+                    props.Embed = builder.Build();
+                });
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error updating message in Discord");
+            }
         }
 
         private async Task OnReady()
         {
-            try
-            {
             _writeChannel = _client.GetChannel(_config.Value.WriteChannelId) as IMessageChannel;
+            if(_writeChannel == null)
+            {
+                _logger.LogWarning("Could not find channel in Discord");
+                return;
+            }
+
             var messages = await GetMessages(_writeChannel);
             if(!messages.Any())
             {
+                _logger.LogInformation("Creating new message for payouts");
                 _message = await _writeChannel.SendMessageAsync(embed: new EmbedBuilder().Build());
             }
             else
@@ -93,14 +109,11 @@ namespace PayoutBot.Discord
                 _message = messages.First() as IUserMessage;
                 if(_message.Embeds.Count == 0)
                 {
+                    _logger.LogInformation("Deleting message without any embeds and creating a new message for payouts");
+
                     await _message.DeleteAsync();
                     _message = await _writeChannel.SendMessageAsync(embed: new EmbedBuilder().Build());
                 }
-            }
-            }
-            catch(Exception ex)
-            {
-                ex = null;
             }
         }
 
